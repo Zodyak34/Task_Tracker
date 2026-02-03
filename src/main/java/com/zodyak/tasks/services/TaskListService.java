c7852fc5ec09340f77fbf4cecc9fe7f3fc1ad3ff
Carson Smith <csm2356@wgu.edu>
2026-02-02 20:01:43 -0600

    Implemented methods to get task lists in TaskListController, TaskListService, and TaskListServiceImpl

--- src/main/java/com/zodyak/tasks/services/TaskListService.java
+++ src/main/java/com/zodyak/tasks/services/TaskListService.java
@@ -3,8 +3,11 @@ package com.zodyak.tasks.services;
 import com.zodyak.tasks.domain.entities.TaskList;
 
 import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
 
 public interface TaskListService {
     List<TaskList> listTaskLists();
     TaskList createTaskList(TaskList taskList);
+    Optional<TaskList> getTaskList(UUID id);
 }
