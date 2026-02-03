235cded281d7756bc0db45ee3cc28968a4b2f50a
Carson Smith <csm2356@wgu.edu>
2026-02-02 20:37:18 -0600

    Implemented methods to create a task in TaskService, TaskServiceImpl, and TaskController

--- src/main/java/com/zodyak/tasks/controllers/TaskController.java
+++ src/main/java/com/zodyak/tasks/controllers/TaskController.java
@@ -1,12 +1,10 @@
 package com.zodyak.tasks.controllers;
 
 import com.zodyak.tasks.domain.dto.TaskDto;
+import com.zodyak.tasks.domain.entities.Task;
 import com.zodyak.tasks.mappers.TaskMapper;
 import com.zodyak.tasks.services.TaskService;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.UUID;
@@ -33,5 +31,18 @@ public class TaskController {
                 .stream()
                 .map(taskMapper::toDto)
                 .toList();
+    }
+
+    @PostMapping
+    public TaskDto createTask(
+            @PathVariable("task_list_id") UUID taskListId,
+            @RequestBody TaskDto taskDto
+    ){
+        Task createdTask = taskService.createTask(
+                taskListId,
+                taskMapper.fromDto(taskDto)
+        );
+
+        return taskMapper.toDto(createdTask);
     }
 }
