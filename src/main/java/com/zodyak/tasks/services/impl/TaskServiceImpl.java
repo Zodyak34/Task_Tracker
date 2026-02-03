2e206ba348345333a231a0b1b5b9296c6a76713e
Carson Smith <csm2356@wgu.edu>
2026-02-02 20:50:37 -0600

    Implemented methods to update a task in TaskService, TaskServiceImpl, and TaskController

--- src/main/java/com/zodyak/tasks/services/impl/TaskServiceImpl.java
+++ src/main/java/com/zodyak/tasks/services/impl/TaskServiceImpl.java
@@ -11,6 +11,7 @@ import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
 import java.util.List;
+import java.util.Objects;
 import java.util.Optional;
 import java.util.UUID;
 
@@ -30,6 +31,7 @@ public class TaskServiceImpl implements TaskService {
     public List<Task> listTasks(UUID taskListId) {
         return taskRepository.findByTaskListId(taskListId);
     }
+
 
     @Override
     public Task createTask(UUID taskListId, Task task) {
@@ -67,5 +69,33 @@ public class TaskServiceImpl implements TaskService {
     @Override
     public Optional<Task> getTask(UUID taskListId, UUID taskId) {
         return taskRepository.findByTaskListIdAndId(taskListId, taskId);
+    }
+
+    @Override
+    public Task updateTask(UUID taskListId, UUID taskId, Task task) {
+        if(null == task.getId()){
+            throw new IllegalArgumentException("Task must have an ID!");
+        }
+        if(!Objects.equals(taskId, task.getId())){
+            throw new IllegalArgumentException("Task IDs don't match!");
+        }
+        if(null == task.getPriority()){
+            throw new IllegalArgumentException("Task must have a priority!");
+        }
+        if(null == task.getStatus()){
+            throw new IllegalArgumentException("Task must have a status!");
+        }
+
+        Task existingTask = taskRepository.findByTaskListIdAndId(taskListId, taskId)
+                .orElseThrow(() -> new IllegalArgumentException("Task not found!"));
+
+        existingTask.setTitle(task.getTitle());
+        existingTask.setDescription(task.getDescription());
+        existingTask.setDueDate(task.getDueDate());
+        existingTask.setPriority(task.getPriority());
+        existingTask.setStatus(task.getStatus());
+        existingTask.setUpdated(LocalDateTime.now());
+
+        return taskRepository.save(existingTask);
     }
 }
